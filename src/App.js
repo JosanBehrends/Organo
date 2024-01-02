5380c56c46a0fad0d0efd4a2fbd477a130d11d85
Josan Behrends <josanbehrends@gmail.com>
2024-01-02 17:54:12 -0300

    Organização dos times

--- src/App.js
+++ src/App.js
@@ -4,6 +4,43 @@ import Formulario from './Componentes/Formulario';
 import Time from './Componentes/Time';
 
 function App() {
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57c278',
+      corSecundaria: '#d9f7e9',
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82cffa',
+      corSecundaria: '#e8fbff',
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#a6d157',
+      corSecundaria: '#F0f8e2',
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#e06b69',
+      corSecundaria: '#fde7e8',
+    },
+    {
+      nome: 'Ux e Design',
+      corPrimaria: '#db6ebf',
+      corSecundaria: '#fae9f5',
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#ffba05',
+      corSecundaria: '#fff5d9',
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#ff8a29',
+      corSecundaria: '#ffeedf',
+    },
+  ]
 
   const [colaboradores, setColaboradores] = useState('')
   const aoNovoColaboradorAdicionado = (colaborador) => {
@@ -16,12 +53,9 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado = {colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-      <Time nome ="Programação"/>
-      <Time nome ="Front-End"/>
-      <Time nome ="Data Science"/>
-      <Time nome ="Programação"/>
+      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/> )}
     </div>
   );
 }
 
-export default App;
+export default App
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,9 +1,11 @@
 import './Time.css'
 
 const Time = (props) => {
+    const corPrimaria = {borderColor: props.corPrimaria}
+    const corSecundaria = {backgroundColor: props.corSecundaria}
     return (
-        <section className='time'>
-            <h3>{props.nome}</h3>
+        <section className='time' style={corSecundaria}>
+            <h3 style={corPrimaria}>{props.nome}</h3>
         </section>
     )
 }
