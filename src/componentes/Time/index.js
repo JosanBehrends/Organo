da7000e2949b44326105790228a97f4bd0d1d60c
Josan Behrends <josanbehrends@gmail.com>
2024-01-02 19:03:19 -0300

    Código depurado e finalizado

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -5,14 +5,12 @@ const Time = (props) => {
     const corPrimaria = {borderColor: props.corPrimaria}
     const corSecundaria = {backgroundColor: props.corSecundaria}
     return (
-        (props.colaboradores.lenght > 0) ? 
-        <section className='time' style={corSecundaria}>
+        (props.colaboradores.length > 0) ? <section className='time' style={corSecundaria}>
             <h3 style={corPrimaria}>{props.nome}</h3>
             <div className='colaboradores'>
-                {props.colaboradores.map( colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/> )}
+                {props.colaboradores.map( colaborador => <Colaborador key={colaborador.nome} corDeFundo={props.corPrimaria} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/> )}
             </div>
-        </section>
-        : ''
+        </section>: ''
     )
 }
 
